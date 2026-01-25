a508d99cde9c574c505a79110633716bf8c798ce
G-PaperChain <g.paperchain@gmail.com>
2026-01-25 17:27:35 +0300

    Add JWT access and refresh secret environment variables

--- server/config/env.ts
+++ server/config/env.ts
@@ -10,3 +10,6 @@ export const DB_URI = process.env.DB_URI as string;
 export const JWT_SECRET = process.env.JWT_SECRET as string;
 export const JWT_EXPIRES_IN =
   process.env.JWT_EXPIRES_IN as SignOptions["expiresIn"];
+
+export const JWT_ACCESS_SECRET = process.env.JWT_ACCESS_SECRET as string;
+export const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET as string;
