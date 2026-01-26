a4b2b660f8ae4d06ab0538ddd700528d64887b20
G-PaperChain <g.paperchain@gmail.com>
2026-01-26 19:35:44 +0300

    Refactor main.tsx to integrate QueryClientProvider and AuthProvider for improved state management and authentication context

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 
 // Components & Pages
@@ -10,6 +9,11 @@ import Homepage from "./pages/HomePage";
 import Dashboard from "./pages/DashboardPage";
 import SignupPage from "./pages/SignupPage";
 import LoginPage from "./pages/LoginPage";
+
+// Provides
+import { AuthProvider } from "./features/auth/AuthContext";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const router = createBrowserRouter([
    {
@@ -22,12 +26,12 @@ const router = createBrowserRouter([
             element: <Homepage />,
          },
          {
-            path : "signup",
-            element: <SignupPage />
+            path: "signup",
+            element: <SignupPage />,
          },
          {
-            path : "login",
-            element: <LoginPage />
+            path: "login",
+            element: <LoginPage />,
          },
          {
             path: "dashboard",
@@ -41,8 +45,14 @@ const router = createBrowserRouter([
    },
 ]);
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
    <StrictMode>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+         <AuthProvider>
+            <RouterProvider router={router} />
+         </AuthProvider>
+      </QueryClientProvider>
    </StrictMode>,
 );
