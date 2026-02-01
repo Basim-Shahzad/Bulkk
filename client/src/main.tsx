35673e8b194ada4e217e52757e2b79943c746239
G-PaperChain <g.paperchain@gmail.com>
2026-02-01 17:31:15 +0300

    add products route

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -14,6 +14,7 @@ import LoginPage from "./pages/LoginPage";
 import { AuthProvider } from "./features/auth/AuthContext";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import ProductsPage from "./pages/ProductsPage";
 
 const router = createBrowserRouter([
    {
@@ -41,6 +42,10 @@ const router = createBrowserRouter([
             path: "inventory",
             element: <div className="p-8">Inventory Module (Coming Soon)</div>,
          },
+         {
+            path: "products",
+            element: <ProductsPage />
+         }
       ],
    },
 ]);
