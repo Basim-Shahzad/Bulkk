e71e05172e26beac9330e4cb5a7e53c9548de359
G-PaperChain <g.paperchain@gmail.com>
2026-02-03 17:44:01 +0300

    feat: added route for updating a product

--- server/src/routes/product.routes.ts
+++ server/src/routes/product.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createProduct, getProductById, getProducts, increaseProductStockById, decreaseProductStockById, deleteProductById } from "../controllers/product.controller";
+import { createProduct, getProductById, getProducts, increaseProductStockById, decreaseProductStockById, deleteProductById, updateProduct } from "../controllers/product.controller";
 import { auth } from "../middleware/auth.middleware";
 
 const productRouter = Router();
@@ -9,6 +9,7 @@ productRouter.post("/products", createProduct);
 productRouter.get("/products", auth,  getProducts);
 productRouter.get("/products/:id", auth, getProductById);
 productRouter.delete("/products/:id", auth, deleteProductById);
+productRouter.patch("/products/:id", auth, updateProduct);
 productRouter.patch("/products/:id/stock-in", auth, increaseProductStockById);
 productRouter.patch("/products/:id/stock-out", auth, decreaseProductStockById);
 
