e2808f6e8d93122c258e0bfa47c76b524b6ec82f
G-PaperChain <g.paperchain@gmail.com>
2026-02-10 13:35:23 +0300

    feat: added more sales related routes

--- server/src/routes/sale.routes.ts
+++ server/src/routes/sale.routes.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
-import { createSale } from "../controllers/sale.controller";
+import { createSale, getSales, getSaleById } from "../controllers/sale.controller";
 import { auth } from "../middleware/auth.middleware";
+import { AdminMiddleware } from "../middleware/admin.middleware";
+import { Admin } from "mongodb";
 
 const salesRouter = Router();
 
 // Public routes
 salesRouter.post("/sales", auth, createSale);
+salesRouter.get("/sales", auth, AdminMiddleware,  getSales);
+salesRouter.get("/sales:id", auth, AdminMiddleware,  getSaleById);
 
 export default salesRouter;
